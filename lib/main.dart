a9878366660216ce513a7d0a5c9c306f7a2291a7
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2024-01-29 10:17:04 +0000

    Display persons, and add, update and delete a person

--- lib/main.dart
+++ lib/main.dart
@@ -94,6 +94,98 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: const Text('Persons')),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final dataModel = ref.watch(personsProvider);
+
+          return ListView.builder(
+            itemCount: dataModel.count,
+            itemBuilder: (context, index) {
+              final person = dataModel.persons[index];
+              return ListTile(
+                onTap: () async {
+                  final updatedPerson = await createOrUpdatePersonDialog(context, person);
+                  if (updatedPerson != null) {
+                    dataModel.update(updatedPerson: updatedPerson);
+                  }
+                },
+                title: Text(person.displayName),
+                trailing: IconButton(
+                  splashRadius: 20,
+                  onPressed: () => dataModel.remove(person: person),
+                  icon: const Icon(Icons.delete_rounded, color: Colors.red),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final person = await createOrUpdatePersonDialog(context);
+          if (person != null) {
+            ref.read(personsProvider).add(person: person);
+          }
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
+}
+
+final nameController = TextEditingController();
+final ageController = TextEditingController();
+
+Future<Person?> createOrUpdatePersonDialog(BuildContext context, [Person? existingPerson]) {
+  String? name = existingPerson?.name;
+  int? age = existingPerson?.age;
+
+  nameController.text = name ?? '';
+  ageController.text = age?.toString() ?? '';
+
+  return showDialog<Person?>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Create a person'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: 'Enter name here...'),
+              onChanged: (value) => name = value,
+            ),
+            TextField(
+              controller: ageController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Enter age here...'),
+              onChanged: (value) => age = int.tryParse(value),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(onPressed: () => Navigator.of(context).pop(), child: const Text('Cancel')),
+          TextButton(
+            onPressed: () {
+              if (name != null && age != null) {
+                if (existingPerson != null) {
+                  // have existing person, update it
+                  final newPerson = existingPerson.updated(name, age);
+                  Navigator.of(context).pop(newPerson);
+                } else {
+                  // no existing person, create a new one
+                  Navigator.of(context).pop(Person(name: name!, age: age!));
+                }
+              } else {
+                // no name, or age or both
+                Navigator.of(context).pop();
+              }
+            },
+            child: const Text('Save'),
+          ),
+        ],
+      );
+    },
+  );
 }
